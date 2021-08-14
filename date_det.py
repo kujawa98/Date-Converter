d5a3a29e7a42daf25706897f8d2666184e023535
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-08-14 13:04:08 +0200

    Created day-month-year regex

---
+++ date_det.py
@@ -0,0 +1,12 @@
+import re
+
+date_dmy_regex = re.compile(r'''
+                        (0?[1-9]|[1-2][0-9]|3[0-1]) #day
+                        /
+                        (0?[1-9]|1[0-2]) #month
+                        /
+                        ([1-2]\d{3}) #year
+                        ''', re.VERBOSE)
+
+
+rg = date_dmy_regex.search('02/02/1998')
