226da2b431d03f8649fea0093a72052b500f764c
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-08-14 13:09:06 +0200

    Created month-day-year and year-day-month regexes

--- date_det.py
+++ date_det.py
@@ -8,5 +8,21 @@ date_dmy_regex = re.compile(r'''
                         ([1-2]\d{3}) #year
                         ''', re.VERBOSE)
 
+date_mdy_regex = re.compile(r'''
+                        (0?[1-9]|1[0-2]) #month
+                        ([/.-])
+                        (0?[1-9]|[1-2][0-9]|3[0-1]) #day
+                        ([/.-])
+                        ([1-2]\d{3}) #year
+                        ''', re.VERBOSE)
+
+date_ydm_regex = re.compile(r'''
+                        ([1-2]\d{3}) #year
+                        ([/.-])
+                        (0?[1-9]|[1-2][0-9]|3[0-1]) #day
+                        ([/.-])
+                        (0?[1-9]|1[0-2]) #month
+                        ''', re.VERBOSE)
+
 rg = date_dmy_regex.search('02/02/1998')
 print(rg.group())
